aba3dc46942078f678264996e8404b12629bc66a
Komorebi Sakuramochi <komorebi_sakuramochi@icloud.com>
2026-01-13 14:39:43 +0900

    Add type definitions and function signatures

--- src/hotaru/hotaru.py
+++ src/hotaru/hotaru.py
@@ -1,2 +1,21 @@
+Board = list[int]
+
+
+def init_board() -> Board:
+    raise NotImplementedError
+
+
+def get_movables_board(board: Board, turn: int, dice: int) -> list[int]:
+    raise NotImplementedError
+
+
+def move_board(board: Board, piece: int, dice: int) -> Board:
+    raise NotImplementedError
+
+
+def pass_board(board: Board) -> Board:
+    raise NotImplementedError
+
+
 def add(x: int, y: int) -> int:
     return x + y
