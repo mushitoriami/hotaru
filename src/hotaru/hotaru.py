e63b9913e780dd20109ba452a4fa70b3d67c72bf
Komorebi Sakuramochi <komorebi_sakuramochi@icloud.com>
2026-01-13 15:37:28 +0900

    Implement `get_movables_board`

--- src/hotaru/hotaru.py
+++ src/hotaru/hotaru.py
@@ -6,7 +6,18 @@ def init_board() -> Board:
 
 
 def get_movables_board(board: Board, turn: int, dice: int) -> list[int]:
-    raise NotImplementedError
+    moves = []
+    for i in range(4):
+        move_from = board[i * 4 + turn]
+        if move_from >= 4:
+            move_to = move_from + dice
+        elif dice == 6:
+            move_to = 4
+        else:
+            continue
+        if move_to <= 47 and move_to not in [board[j * 4 + turn] for j in range(4)]:
+            moves.append(i)
+    return moves
 
 
 def move_board(board: Board, piece: int, dice: int) -> Board:
--- tests/test_hotaru.py
+++ tests/test_hotaru.py
@@ -1,8 +1,32 @@
-from hotaru.hotaru import add, init_board
+from hotaru.hotaru import add, init_board, get_movables_board
 
 
 def test_init_board() -> None:
     assert init_board() == [0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3]
+
+
+def test_board_1() -> None:
+    board = [46, 0, 0, 0, 1, 1, 1, 1, 8, 2, 2, 2, 10, 3, 3, 3]
+    dice, turn = 2, 0
+    assert get_movables_board(board, turn, dice) == [3]
+
+
+def test_board_2() -> None:
+    board = [10, 0, 0, 0, 4, 1, 1, 1, 2, 2, 2, 2, 43, 3, 3, 3]
+    dice, turn = 6, 0
+    assert get_movables_board(board, turn, dice) == [0]
+
+
+def test_board_3() -> None:
+    board = [0, 0, 0, 0, 7, 34, 1, 1, 46, 2, 2, 2, 15, 3, 3, 19]
+    dice, turn = 2, 0
+    assert get_movables_board(board, turn, dice) == [1, 3]
+
+
+def test_board_4() -> None:
+    board = [13, 0, 0, 0, 43, 1, 1, 29, 2, 24, 2, 2, 3, 3, 3, 3]
+    dice, turn = 6, 0
+    assert get_movables_board(board, turn, dice) == [0, 2, 3]
 
 
 def test_add() -> None:
